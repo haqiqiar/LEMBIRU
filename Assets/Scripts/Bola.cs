c113ffd319ef33005f8491b020b1d7b46df9ba65
RIZ-PC\haqiqi <rzkhqq@gmail.com>
2016-12-18 00:40:52 +0700

    Starting until throwing ball success

---
+++ Assets/Scripts/Bola.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bola : MonoBehaviour {
+
+    GameObject prefab;
+	// Use this for initialization
+	void Start () {
+        prefab = Resources.Load("Bola") as GameObject;
+	}
+
+    
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject projectile = Instantiate(prefab) as GameObject;
+            Object.Destroy(projectile, 3.0f);
+            projectile.transform.position = transform.position + Camera.main.transform.forward * 300;
+            
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+            rb.velocity = Camera.main.transform.forward * 800;
+            
+        }
+        
+
+    }
+}
---
+++ Assets/Scripts/onBall.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class onBall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+
+
+        if (col.collider.name == "inside")
+        {
+
+            Destroy(gameObject);
+        }
+
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
